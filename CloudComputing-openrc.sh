3488a763ed0ee3800a4bd77f3de379d2c38b4020
rafa20506 <69425446+rafa20506@users.noreply.github.com>
2021-12-19 17:20:56 -0400

    archivo para linea de comandos

--- CloudComputing-openrc.sh
+++ CloudComputing-openrc.sh
@@ -10,10 +10,10 @@
 # OpenStack API is version 3. For example, your cloud provider may implement
 # Image API v1.1, Block Storage API v2, and Compute API v2.0. OS_AUTH_URL is
 # only for the Identity API served through keystone.
-export OS_AUTH_URL=http://192.168.109.129/identity
+export OS_AUTH_URL=http://192.168.18.128/identity
 # With the addition of Keystone we have standardized on the term **project**
 # as the entity that owns the resources.
-export OS_PROJECT_ID=38e264cbde774ee699767d4243e55541
+export OS_PROJECT_ID=eacdf095dd5f4919ae2a936b73c08b8a
 export OS_PROJECT_NAME="CloudComputing"
 export OS_USER_DOMAIN_NAME="Default"
 if [ -z "$OS_USER_DOMAIN_NAME" ]; then unset OS_USER_DOMAIN_NAME; fi
